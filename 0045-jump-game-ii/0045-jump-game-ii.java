0fa3fd3c6f00df4ac56e8edb83f9aebd400c602e
AnkitVerma-a <ankitverma13042004@gmail.com>
2026-07-07 15:40:57 +0530

    Time: 0 ms (100%), Space: 47.3 MB (38.54%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,24 +1,28 @@
 class Solution {
-    public int f(int i,int[] nums,int[] dp){
-        if(i==0){
-            return 0;
+    public int findmax(int l, int r, int[] nums)
+    {
+        int max=0;
+        for(int i=l;i<=r;i++)
+        {
+            max=Math.max(max,i+nums[i]);
         }
-        int min = Integer.MAX_VALUE;
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        for(int j=0;j<i;j++){
-            if(j+nums[j]>=i){
-                int steps = 1+f(j,nums,dp);
-                min=Math.min(min,steps);
-            }
-        }
-        return dp[i]= min;
-        
+        return max;
     }
     public int jump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return f(nums.length-1,nums,dp);
+        if(nums.length==1)
+        {
+            return 0;
+        }
+        int l=0;
+        int r=0;
+        int jumps=0;
+        while(r<nums.length-1)
+        {
+            jumps++;
+            int max=findmax(l,r,nums);
+            l=r+1;
+            r=max;
+        }
+        return jumps;
     }
 }
