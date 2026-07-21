b8943e99a75375876f26604aef2965723a6536b2
AnkitVerma-a <ankitverma13042004@gmail.com>
2026-07-21 17:27:32 +0530

    Time: 2 ms (99.6%), Space: 43.1 MB (94.14%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,28 +1,21 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        int[] arr = {2,3,5};
-        int[] count ={0,0,0};
+        int[] arr = {2, 3, 5};
+        int[] pointers = {0, 0, 0}; 
         int[] a = new int[n];
-        a[0]=1;
-        int j=1;
-        while(j!=n){
-            int minVal = Integer.MAX_VALUE;
-            int minIndex = -1;
-            for (int i = 0; i < 3; i++) {
-            int product = arr[i] * a[count[i]];
-            if (product < minVal) {
-                minVal = product;
-                minIndex = i;
-            }
-            }
-            a[j]=minVal;;
-            for(int i = 0; i < 3; i++){
-                if (arr[i] * a[count[i]] == minVal) {
-                    count[i]++;
-                }
-            }
+        a[0] = 1;
+        int j = 1;
+        while (j < n) {
+            int v0 = a[pointers[0]] * arr[0];
+            int v1 = a[pointers[1]] * arr[1];
+            int v2 = a[pointers[2]] * arr[2];
+            int minVal = Math.min(v0, Math.min(v1, v2));
+            a[j] = minVal;
+            if (minVal == v0) pointers[0]++;
+            if (minVal == v1) pointers[1]++;
+            if (minVal == v2) pointers[2]++;
             j++;
         }
-        return a[n-1];
+        return a[n - 1];
     }
 }
