b6d33ec511081f520b4e0bfb7e26ccb583c4d88e
AnkitVerma-a <ankitverma13042004@gmail.com>
2026-08-02 12:32:11 +0530

    Time: 14 ms (29.47%), Space: 46.6 MB (17.16%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str=s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int low=0;
+        int high=str.length()-1;
+        while(low<high){
+            if(str.charAt(low)!=str.charAt(high)){
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
+    }
+}
