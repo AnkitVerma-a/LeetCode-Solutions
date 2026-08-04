3e23aacc13c050387971b49d93e9b93605a9a675
AnkitVerma-a <ankitverma13042004@gmail.com>
2026-08-04 12:17:04 +0530

    Time: 1 ms (100%), Space: 45.7 MB (51.64%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,22 @@
 class Solution {
-    public void f(int idx,List<Integer> ls,int[] arr,int k,int sum,List<List<Integer>> ls1){
-        if(sum>k){
-            return;
-        }
-        if(idx==arr.length||sum==k){
-            if(sum==k){
+    public void f(int idx,List<Integer> ls,int[] arr,int sum,List<List<Integer>> ls1){
+        if(idx==arr.length || sum==0){
+            if(sum==0){
                 ls1.add(new ArrayList<>(ls));
             }
             return;
         }
-        ls.add(arr[idx]);
-        f(idx,ls,arr,k,sum+arr[idx],ls1);
-        ls.remove(ls.size()-1);
-        f(idx+1,ls,arr,k,sum,ls1);
+        if(arr[idx]<=sum){
+            ls.add(arr[idx]);
+            f(idx,ls,arr,sum-arr[idx],ls1);
+            ls.remove(ls.size()-1);
+        }
+        f(idx+1,ls,arr,sum,ls1);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<Integer> ls = new ArrayList<>();
         List<List<Integer>> ls1=new ArrayList<>();
-        f(0,ls,candidates,target,0,ls1);
+        f(0,ls,candidates,target,ls1);
         return ls1;
     }
 }
