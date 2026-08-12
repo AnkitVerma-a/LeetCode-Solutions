809858170ce97827e0f55819623f3ec85ba8a3dd
AnkitVerma-a <ankitverma13042004@gmail.com>
2026-08-12 19:32:53 +0530

    Time: 1 ms (99.94%), Space: 46.9 MB (46.56%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int j=0;
-        for(int i=1;i<nums.length;i+=2){
-            if(nums[i]==nums[j]){
-                j+=2;
-            }
+        int unique=0;
+        for(int i=0;i<nums.length;i++){
+            unique=unique^nums[i];
         }
-        return nums[j];
+        return unique;
     }
 }
