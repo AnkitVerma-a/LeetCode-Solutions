158f2f19a79f316b479cd2d084f1f282cb363280
AnkitVerma-a <ankitverma13042004@gmail.com>
2026-08-18 20:06:11 +0530

    Time: 16 ms (99.83%), Space: 79.8 MB (80.28%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,23 +1,49 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        Set<Integer> set=new HashSet<>();
-        for(int num:nums){
-            set.add(num);
-        }
+    public static int longestConsecutive(int[] nums) {
         int MaxS=0;
-        for(int num:set){
-            if(!set.contains(num-1)){
-                int current=num;
-                int sum=0;
-                while(set.contains(current)){
-                    current++;
+        int sum=0;
+        if(nums.length==0){
+            return 0;
+        }
+        int min=nums[0];
+        int max=nums[0];
+        for(int num:nums){
+            if(num<min)min=num;
+            if(num>max)max=num;
+        }
+        int range=max-min+1;
+        if(range<=100000){
+            boolean[] present=new boolean[range];
+            for(int num:nums){
+                present[num-min]=true;
+            }
+            for(boolean isPresent:present){
+                if(isPresent){
                     sum++;
-                }
-                if(sum>MaxS){
-                    MaxS=sum;
+                }else{
+                    MaxS=Math.max(MaxS,sum);
+                    sum=0;
                 }
             }
+        return Math.max(MaxS, sum);
+        }else{
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num);
+            }
+            
+            for (int num : set) {
+                if (!set.contains(num - 1)) {
+                    int current = num;
+                    int localSum = 0;
+                    while (set.contains(current)) {
+                        current++;
+                        localSum++;
+                    }
+                    MaxS = Math.max(MaxS, localSum);
+                }
+            }
+            return MaxS;
         }
-        return MaxS;
     }
 }
