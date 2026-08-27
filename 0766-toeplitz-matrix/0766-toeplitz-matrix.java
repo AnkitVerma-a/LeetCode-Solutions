fab853fc67dc336e886192934f9254f17025da9b
AnkitVerma-a <ankitverma13042004@gmail.com>
2026-08-27 22:54:49 +0530

    Time: 0 ms (100%), Space: 46.5 MB (47.3%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -4,15 +4,10 @@ class Solution {
         Map<Integer,Integer> map=new HashMap<>();
         int m=matrix.length;
         int n=matrix[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int val=m+n+j-i;
-                if(map.containsKey(val)){
-                    if(map.get(val)!=matrix[i][j]){
-                        return false;
-                    }
-                }else{
-                    map.put(val,matrix[i][j]);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]!=matrix[i-1][j-1]){
+                    return false;
                 }
             }
         }
